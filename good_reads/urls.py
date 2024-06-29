a0c063f2a02733ca53e00ab100af19a3c48aee44
Davronbekatadjanov <atadjanovdavronbek863@gmail.com>
2024-06-29 10:42:52 +0500

    goodreads projects create 1) book model added 2) User app added 3) view, urls added

---
+++ good_reads/urls.py
@@ -0,0 +1,15 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+from good_reads.view import landing_page
+
+urlpatterns = [
+    path("", landing_page, name="landing_page"),
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls"), name="users"),
+
+]
+
+
