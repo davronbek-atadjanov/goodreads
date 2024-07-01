6e530ae797d3a975908181a7dd11ac932f6fbb7b
Davronbekatadjanov <atadjanovdavronbek863@gmail.com>
2024-07-01 17:37:19 +0500

    Static file added

--- good_reads/settings.py
+++ good_reads/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-MEDIA_URL = "/media/"
-STATIC_URL = "static/"
 MEDIA_ROOT = "media"
+MEDIA_URL = "/media/"
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
