128e833d3df3891ea7383c7c7aa473bcbafad440
Davronbekatadjanov <atadjanovdavronbek863@gmail.com>
2024-07-07 16:49:43 +0500

    Home Page and HomePageTestCase added

--- good_reads/urls.py
+++ good_reads/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from good_reads import settings
-from good_reads.view import landing_page
-
+from good_reads.view import landing_page, HomePageView
 urlpatterns = [
     path("", landing_page, name="landing_page"),
+    path("home/", HomePageView.as_view(), name="home_page"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("books/", include("books.urls"))
--- good_reads/view.py
+++ good_reads/view.py
@@ -1,6 +1,27 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+
+from books.models import BookReview
 
 
 def landing_page(request):
     return render(request, "landing.html")
+
+class HomePageView(View):
+
+    def get(self, request):
+        book_reviews = BookReview.objects.all().order_by("-created_at")
+
+        page_size = request.GET.get('page_size', 5)
+        paginator = Paginator(book_reviews, page_size)
+        page_num = request.GET.get('page', 1)
+        page_object = paginator.get_page(page_num)
+
+        context = {
+            "page_obj": page_object
+        }
+        return render(request, "home.html", context)
+
+
